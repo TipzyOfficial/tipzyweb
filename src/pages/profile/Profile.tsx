bd1633d6dfbbce56bb78bf7bfd3b0b63d63e8df1
James Koga <jyk54@cornell.edu>
2024-05-29 20:52:50 +0200

    Added basic functionality for default profile image
    Added basic functionality for default profile image

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -21,21 +21,31 @@ import TZHeader from "../../components/TZHeader";
  * 
  */
 
-function ProfileItem(props: { title: string, value: string, onClick?: () => void }) {
+const defaultImage = "src/pages/profile/tempProfPic.png";
+
+
+function ProfileItem(props: { title: string, value: string, profilePic?: string, onClick?: () => void }) {
+    console.log("Profile Name is: " + props.value);
     return (
-        <div style={{ padding: padding, width: "100%", backgroundColor: "red", display: "flex" }}>
+
+        <div style={{ padding: padding, width: "100%", backgroundColor: "red", display: "flex", }}>
             <span style={{ backgroundColor: "purple", padding: padding }}>
                 <div style={{ backgroundColor: "pink", borderRadius: "50%" }}>
-                    <img></img>
+                    {props.profilePic ? (
+                        <img src={props.profilePic} alt="Profile" style={{ borderRadius: "50%" }} ></img>)
+                        :
+                        (<div style={defaultProfile}>{props.value.substring(0, 0) + props.value.substring(props.value.length - 1, props.value.length - 1)}</div>)
+                    }
+
                 </div>
-            </span>
+            </span >
             <span style={{ backgroundColor: "blue", padding: padding }}>
                 {props.title}:
             </span>
             <span style={{ backgroundColor: "orange", padding: padding }}>
                 {props.value}
             </span>
-        </div>
+        </div >
     );
 }
 
@@ -43,7 +53,7 @@ export default function Profile() {
     const usc = useContext(UserSessionContext)
     const user = usc.user;
     const cookies = getCookies();
-
+    const profilePic = user.image;
     // user.name
     // user.email
 
@@ -51,7 +61,7 @@ export default function Profile() {
         <div className={"App-body-top"}>
             <TZHeader title="Your Profile" />
             <div style={styles}>
-                <ProfileItem title="Name" value={usc.user.name}></ProfileItem>
+                <ProfileItem title="Name" value={usc.user.name} profilePic={user.image}></ProfileItem>
                 <TZButton title={"logout"} onClick={() => Logout(cookies)}></TZButton>
             </div>
         </div>
@@ -62,4 +72,11 @@ const styles: React.CSSProperties = {
     paddingRight: padding,
     paddingLeft: padding,
     width: "100%"
+}
+
+const defaultProfile: React.CSSProperties = {
+    width: "50px",
+    height: "50px",
+    background: 'linear-gradient(0deg, #c76b89, #d38932)',
+    borderRadius: '50%'
 }
