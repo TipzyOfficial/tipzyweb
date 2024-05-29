89ec56f0a8b1a87c751ebde8eae2597e9ef010f3
Gordi Tenev <150630157+TipzyGordi@users.noreply.github.com>
2024-05-29 02:29:22 -0400

    fixed bug with elements not loading sometimes

--- src/pages/profile/PaymentSetup.tsx
+++ src/pages/profile/PaymentSetup.tsx
@@ -1,10 +1,52 @@
-import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { FormEvent } from "react";
-import { Button } from "react-bootstrap";
+import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { FormEvent, useContext, useEffect, useState } from "react";
+import { Button, Spinner } from "react-bootstrap";
 import TZButton from "../../components/TZButton";
-import { padding } from "../../lib/Constants";
+import { Colors, padding } from "../../lib/Constants";
+import { UserSessionContext } from "../../lib/UserSessionContext";
+import { fetchPaymentSheetParams } from "../../lib/stripe";
+import { Logout } from "../..";
+import { getCookies, stripePromise } from "../../App";
+import { DisplayOrLoading } from "../../components/DisplayOrLoading";
 
 export default function PaymentSetup() {
+    const [clientSecret, setClientSecret] = useState<string | undefined | null>(undefined);
+    const usc = useContext(UserSessionContext);
+
+    useEffect(() => {
+        // Create SetupIntent as soon as the page loads
+        fetchPaymentSheetParams(usc.user).then(
+        (r) => {
+            setClientSecret(r);
+            console.log("ClientSecret", r)
+        }
+        )
+    }, []);
+
+    if(clientSecret === null) {
+        Logout(getCookies());
+        return(<></>)
+    }
+
+    return(
+        clientSecret ? 
+        <Elements 
+        stripe={stripePromise} 
+        options={{
+          clientSecret: clientSecret,
+          appearance: {theme: "night"}
+        }}
+        >
+            <PaymentSetupInner/>
+        </Elements>
+        : 
+        <div style={{padding: 20, display: 'flex', justifyContent: 'center'}}>
+            <Spinner style={{color: Colors.primaryRegular, width: 75, height: 75}}></Spinner>
+        </div>
+    )
+}
+
+function PaymentSetupInner() {
     const stripe = useStripe();
     const elements = useElements();
 
@@ -36,7 +78,20 @@ export default function PaymentSetup() {
         // site first to authorize the payment, then redirected to the `return_url`.
         }
     };
-
+    
+    // return(
+    //     clientSecret ? 
+    //     <Elements 
+    //     stripe={stripePromise} 
+    //     options={{
+    //         clientSecret: clientSecret,
+    //         appearance: {theme: "night"}
+    //     }}
+    //     >
+    //         <Outlet/>
+    //     </Elements> : 
+    //     <DisplayOrLoading condition={false}></DisplayOrLoading>
+    // );
 
     return(
         <form onSubmit={handleSubmit}>
