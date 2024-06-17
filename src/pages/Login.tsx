bb38de7f9676311942bb43bb5c01a23a1b130618
Gordi Tenev <150630157+TipzyGordi@users.noreply.github.com>
2024-06-17 01:55:38 -0400

    try it out

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,6 +13,7 @@ import Register from './Register';
 import { Colors, padding } from '../lib/Constants';
 import { Spinner } from 'react-bootstrap';
 import { getCookies } from '../lib/utils';
+import AppleLogin from 'react-apple-login'
 
 const formatBirthday = (birthday: Date) => {
     return `${birthday.getFullYear()}-${birthday.getMonth() + 1 >= 10 ? (birthday.getMonth() + 1) : "0" + (birthday.getMonth() + 1)}-${birthday.getDate() >= 10 ? birthday.getDate() : "0" + birthday.getDate()}`
@@ -283,6 +284,32 @@ function Login(props: { back?: boolean }) {
                         <BigLogo></BigLogo>
                     </div>
                     <div style={{ justifyContent: 'center', alignItems: 'center' }}>
+                        <AppleLogin
+                            clientId="app.tipzy.TipzyAppleSignIn"
+                            redirectURI="https://app.tipzy.app"
+                            usePopup={true}
+                            callback={(response) => {
+                                console.log("response from apple: ", response);
+                            }} // Catch the response
+                            scope="email name"
+                            responseMode="query"
+                        // render={renderProps => (  //Custom Apple Sign in Button
+                        //     <button
+                        //         onClick={renderProps.onClick}
+                        //         style={{
+                        //             backgroundColor: "white",
+                        //             padding: 10,
+                        //             border: "1px solid black",
+                        //             fontFamily: "none",
+                        //             lineHeight: "25px",
+                        //             fontSize: "25px"
+                        //         }}
+                        //     >
+                        //         <i className="fa-brands fa-apple px-2 "></i>
+                        //         Continue with Apple
+                        //     </button>
+                        // )}
+                        />
                         <GoogleButton
                             style={{ width: '100%' }}
                             onClick={() => googleLogin()} />
