e02ae0b765feeacb25d08c80d7e9f6540c569026
Gordi Tenev <150630157+TipzyGordi@users.noreply.github.com>
2024-06-17 04:57:02 -0400

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -43,8 +43,6 @@ function Login(props: { back?: boolean }) {
 
         loginWithAppleAccessToken(accessToken).then((value) => login(value.access_token, value.refresh_token, value.expires_at)).catch(
             (e: Error) => {
-                console.log("token", accessToken);
-                console.log("cid", process.env.REACT_APP_APPLE_CLIENT_ID, process.env.REACT_APP_APPLE_CLIENT_SECRET);
                 console.log(`Error logging into Tipzy servers via Apple:`, `${e}`);
                 setGlobalDisable(false);
             })
