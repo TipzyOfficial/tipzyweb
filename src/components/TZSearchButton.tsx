5bb6248d378e10c4265fea812457f029ee824f08
Gordi Tenev <150630157+TipzyGordi@users.noreply.github.com>
2024-07-10 13:00:30 -0400

    tenley likes right better

--- src/components/TZSearchButton.tsx
+++ src/components/TZSearchButton.tsx
@@ -29,7 +29,7 @@ export default function TZSearchButton(props: { onClick: () => void, dims?: numb
                     fontWeight: "bold",
                     color: "transparent",//Colors.primaryRegular,
                     // background: 'linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722)',
-                    backgroundImage: `linear-gradient(to bottom, ${Colors.primaryRegular}, ${Colors.secondaryDark})`,
+                    backgroundImage: `linear-gradient(to right, ${Colors.primaryRegular}, ${Colors.secondaryDark})`,
                     backgroundClip: "text",
                     // WebkitBackgroundClip: 'text',
                     // WebkitTextFillColor: 'transparent',
